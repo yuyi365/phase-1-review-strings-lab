07a4998c8faa59cfaa15b3c49eb99fe483e90580
Yuyi <yuyi365@gmail.com>
2021-10-12 16:37:21 -0400

    commit tree 15a46884a10bf9c457d1d7867a4dea774772a814

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+
+const currentUser = 'Hamster';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
